b6a1f9ffe9b0b84093e9e474d5b85919fcf7cae7
Lowe0 <45861748+Lowe0@users.noreply.github.com>
2024-03-15 18:47:31 -0400

    make REST endpoint

--- src/main/java/com/example/spring6restmvc/controllers/BeerController.java
+++ src/main/java/com/example/spring6restmvc/controllers/BeerController.java
@@ -5,16 +5,19 @@ import com.example.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
 public class BeerController {
     private final BeerService beerService;
 
+    @RequestMapping("/api/v1/beers")
     public Iterable<Beer> listBeers() {
         return beerService.listBeers();
     }

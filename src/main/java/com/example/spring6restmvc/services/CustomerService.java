57ca3b7f088eb7ad42883d8ba6063cc18b9fa68d
Lowe0 <45861748+Lowe0@users.noreply.github.com>
2024-03-15 19:14:51 -0400

    implement Customer controller & stack

---
+++ src/main/java/com/example/spring6restmvc/services/CustomerService.java
@@ -0,0 +1,11 @@
+package com.example.spring6restmvc.services;
+
+import com.example.spring6restmvc.model.Customer;
+
+import java.util.UUID;
+
+public interface CustomerService {
+    Iterable<Customer> listCustomers();
+
+    Customer getCustomerById(UUID id);
+}
---
+++ src/main/java/com/example/spring6restmvc/services/CustomerServiceImpl.java
@@ -0,0 +1,54 @@
+package com.example.spring6restmvc.services;
+
+import com.example.spring6restmvc.model.Customer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Slf4j
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    private Map<UUID, Customer> customerMap;
+
+    public CustomerServiceImpl() {
+        populateMap();
+    }
+
+    private void populateMap() {
+        this.customerMap = new HashMap<>();
+
+        Customer kevin = Customer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .customerName("Kevin Lowe")
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now()).build();
+
+        Customer cassi = Customer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .customerName("Cassi Lowe")
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now()).build();
+
+        customerMap.put(kevin.getId(), kevin);
+        customerMap.put(cassi.getId(), cassi);
+    }
+
+    @Override
+    public Iterable<Customer> listCustomers() {
+        log.debug("Customer Service - Get Customers");
+        return new ArrayList<>(customerMap.values());
+    }
+
+    @Override
+    public Customer getCustomerById(UUID id) {
+        log.debug("Customer Service - Get Customer - " + id);
+        return customerMap.get(id);
+    }
+}

32c7ef3ccc1de96bf683d467f81d852aceb4438c
Lowe0 <45861748+Lowe0@users.noreply.github.com>
2024-03-15 18:57:22 -0400

    make get by ID path-relative

--- src/main/java/com/example/spring6restmvc/controllers/BeerController.java
+++ src/main/java/com/example/spring6restmvc/controllers/BeerController.java
@@ -5,7 +5,9 @@ import com.example.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -14,15 +16,17 @@ import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    @RequestMapping(method = RequestMethod.GET)
     public Iterable<Beer> listBeers() {
         return beerService.listBeers();
     }
 
-    public Beer getBeerById(UUID id) {
+    @RequestMapping(value = "/{beerId}", method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beerId") UUID id) {
         log.debug("Get Beer by ID - controller");
         return beerService.getBeerByUUID(id);
     }

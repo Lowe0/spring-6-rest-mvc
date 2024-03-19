8dd13d37ea338b978438d2d324c1ee87a2164007
Lowe0 <45861748+Lowe0@users.noreply.github.com>
2024-03-19 11:15:21 -0400

    Implement MockMvc testing

--- src/test/java/com/example/spring6restmvc/controllers/BeerControllerTest.java
+++ src/test/java/com/example/spring6restmvc/controllers/BeerControllerTest.java
@@ -1,21 +1,40 @@
 package com.example.spring6restmvc.controllers;
 
+import com.example.spring6restmvc.model.Beer;
+import com.example.spring6restmvc.services.BeerService;
+import com.example.spring6restmvc.services.BeerServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.UUID;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
+@WebMvcTest(BeerController.class)
 class BeerControllerTest {
 
     @Autowired
-    BeerController controller;
+    MockMvc mockMvc;
+
+    @MockBean
+    BeerService beerService;
+
+    BeerServiceImpl serviceImpl = new BeerServiceImpl();
 
     @Test
-    void getBeerById() {
-        System.out.println(controller.getBeerById(new UUID(0,0)));
+    void getBeerById() throws Exception {
+        Beer testBeer = serviceImpl.listBeers().iterator().next();
+
+        given(beerService.getBeerByUUID(testBeer.getId())).willReturn(testBeer);
+
+        mockMvc.perform(get("/api/v1/beer/" + testBeer.getId())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(testBeer.getId().toString()));
     }
 }

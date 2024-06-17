261b98206c174601b88037d5ea42ab1d75cf9de6
Lowe0 <45861748+Lowe0@users.noreply.github.com>
2024-06-16 20:51:04 -0400

    Add a Beer Controller integration test

---
+++ src/test/java/com/example/spring6restmvc/controllers/BeerControllerIntegrationTest.java
@@ -0,0 +1,27 @@
+package com.example.spring6restmvc.controllers;
+
+import com.example.spring6restmvc.model.BeerDto;
+import com.example.spring6restmvc.repositories.BeerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BeerControllerIntegrationTest {
+    @Autowired
+    BeerController beerController;
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void listBeers() {
+        Iterable<BeerDto> beerDtos = beerController.listBeers();
+        assertNotNull(beerDtos);
+    }
+}

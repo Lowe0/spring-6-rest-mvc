3d13136ea267712c9f8c8e6c8fd47f4072654a4d
Lowe0 <45861748+Lowe0@users.noreply.github.com>
2024-06-16 21:01:53 -0400

    Implement test for empty Beer table

--- src/test/java/com/example/spring6restmvc/controllers/BeerControllerIntegrationTest.java
+++ src/test/java/com/example/spring6restmvc/controllers/BeerControllerIntegrationTest.java
@@ -5,8 +5,9 @@ import com.example.spring6restmvc.repositories.BeerRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.LinkedList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,5 +25,16 @@ class BeerControllerIntegrationTest {
         List<BeerDto> beerDtos = beerController.listBeers();
         assertNotNull(beerDtos);
         assertThat(beerDtos.size()).isEqualTo(3);
+    }
+
+    @Transactional
+    @Rollback
+    @Test
+    void listBeers_Empty() {
+        beerRepository.deleteAll();
+        List<BeerDto> beerDtos = beerController.listBeers();
+        assertNotNull(beerDtos);
+        assertThat(beerDtos.size()).isEqualTo(0);
+
     }
 }

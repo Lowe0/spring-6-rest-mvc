e8b8a070fabec962e03514c1e0944bebcfb4f862
Lowe0 <45861748+Lowe0@users.noreply.github.com>
2024-06-16 20:25:36 -0400

    Assignment - add a runner to initialize the in-memory database

---
+++ src/main/java/com/example/spring6restmvc/bootstrap/BootstrapData.java
@@ -0,0 +1,81 @@
+package com.example.spring6restmvc.bootstrap;
+
+import com.example.spring6restmvc.entities.Beer;
+import com.example.spring6restmvc.entities.Customer;
+import com.example.spring6restmvc.model.BeerStyle;
+import com.example.spring6restmvc.repositories.BeerRepository;
+import com.example.spring6restmvc.repositories.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class BootstrapData implements CommandLineRunner {
+    private final BeerRepository beerRepository;
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        populateBeer();
+        populateCustomer();
+    }
+
+    private void populateBeer() {
+        if (beerRepository.count() == 0) {
+            Beer sunlight = Beer.builder()
+                    .beerName("Sunlight")
+                    .beerStyle(BeerStyle.CREAM_ALE)
+                    .upc("01 11111 00000 10")
+                    .price(new BigDecimal("12.99"))
+                    .quantityOnHand(99)
+                    .createdDate(LocalDateTime.now())
+                    .updatedDate(LocalDateTime.now())
+                    .build();
+
+            Beer osiris = Beer.builder()
+                    .beerName("Osiris")
+                    .beerStyle(BeerStyle.IPA)
+                    .upc("01 11111 00002 11")
+                    .price(new BigDecimal("12.99"))
+                    .quantityOnHand(99)
+                    .createdDate(LocalDateTime.now())
+                    .updatedDate(LocalDateTime.now())
+                    .build();
+
+            Beer weeMac = Beer.builder()
+                    .beerName("Wee Mac")
+                    .beerStyle(BeerStyle.SCOTTISH_ALE)
+                    .upc("01 11111 00003 20")
+                    .price(new BigDecimal("12.99"))
+                    .quantityOnHand(99)
+                    .createdDate(LocalDateTime.now())
+                    .updatedDate(LocalDateTime.now())
+                    .build();
+
+            beerRepository.save(sunlight);
+            beerRepository.save(osiris);
+            beerRepository.save(weeMac);
+        }
+    }
+
+    private void populateCustomer() {
+        if (customerRepository.count() == 0) {
+            Customer kevin = Customer.builder()
+                    .customerName("Kevin Lowe")
+                    .createdDate(LocalDateTime.now())
+                    .lastModifiedDate(LocalDateTime.now()).build();
+
+            Customer cassi = Customer.builder()
+                    .customerName("Cassi Lowe")
+                    .createdDate(LocalDateTime.now())
+                    .lastModifiedDate(LocalDateTime.now()).build();
+
+            customerRepository.save(kevin);
+            customerRepository.save(cassi);
+        }
+    }
+}

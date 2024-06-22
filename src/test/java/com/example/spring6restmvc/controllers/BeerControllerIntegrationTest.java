a3783cc5263405b97827e14fdff87168d5e7bc26
Lowe0 <45861748+Lowe0@users.noreply.github.com>
2024-06-22 16:39:49 -0400

    Touch up test before moving on

--- src/test/java/com/example/spring6restmvc/controllers/BeerControllerIntegrationTest.java
+++ src/test/java/com/example/spring6restmvc/controllers/BeerControllerIntegrationTest.java
@@ -12,7 +12,6 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -56,6 +55,8 @@ class BeerControllerIntegrationTest {
         assertThrows(NotFoundException.class, () -> beerController.getBeerById(UUID.randomUUID()));
     }
 
+    @Transactional
+    @Rollback
     @Test
     void addBeer() {
         BeerDto toAdd = BeerDto.builder()
@@ -67,8 +68,9 @@ class BeerControllerIntegrationTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
         assertThat(response.getHeaders()).containsKey("Location");
 
-        String uuid = (String) response.getHeaders().getLocation().getPath().split("/")[4];
+        String uuid = response.getHeaders().getLocation().getPath().split("/")[4];
         Beer addedBeer = beerRepository.findById(UUID.fromString(uuid)).get();
         assertThat(addedBeer).isNotNull();
+        assertThat(addedBeer.getBeerName()).isEqualTo(toAdd.getBeerName());
     }
 }

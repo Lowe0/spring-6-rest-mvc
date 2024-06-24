bb6ba006759377ec34c3d1bdfe185d80f1097c31
Lowe0 <45861748+Lowe0@users.noreply.github.com>
2024-06-24 16:08:23 -0400

    Add delta method to beer mapper

--- src/main/java/com/example/spring6restmvc/mappers/BeerMapper.java
+++ src/main/java/com/example/spring6restmvc/mappers/BeerMapper.java
@@ -2,10 +2,16 @@ package com.example.spring6restmvc.mappers;
 
 import com.example.spring6restmvc.entities.Beer;
 import com.example.spring6restmvc.model.BeerDto;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper
 public interface BeerMapper {
     Beer beerDtoToBeer(BeerDto beerDto);
     BeerDto beerToBeerDto(Beer beer);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void deltaBeerFromBeerDto(BeerDto beerDto, @MappingTarget Beer beer);
 }

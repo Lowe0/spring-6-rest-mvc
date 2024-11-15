3832fd1aeb6738edc665b929c05493668bd5c2b1
Lowe0 <45861748+Lowe0@users.noreply.github.com>
2024-11-15 15:21:50 -0500

    Make Repository test a full context test
    I disagree with this, but it's what the course is doing.

--- src/test/java/com/example/spring6restmvc/repositories/BeerOrderRepositoryTest.java
+++ src/test/java/com/example/spring6restmvc/repositories/BeerOrderRepositoryTest.java
@@ -7,11 +7,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
+@SpringBootTest
 class BeerOrderRepositoryTest {
     @Autowired
     BeerOrderRepository beerOrderRepository;
